967bfb784970564fda380a2043fd3352a868bf73
JonZeta <5842564+JonZeta@users.noreply.github.com>
2023-02-28 21:02:27 +0100

    Add process porcentage
    Add process porcentage

--- converter_for_studysmarter.py
+++ converter_for_studysmarter.py
@@ -43,7 +43,7 @@ reader = PdfReader(args.pdf)
 texto_completo = ""
 for page in reader.pages:
     texto_completo += page.extract_text() + "\n"
-print(texto_completo)
+#print(texto_completo)
 texto_completo = texto_completo.replace("\"","''")
 
 #Borrar todo el texto que haya antes de la primera pregunta, si no hay texto borrará la primera pregunta.
@@ -54,8 +54,6 @@ if match:
 
 #Borrar el texto de fin de página.
 texto_completo = re.sub(r'(?s)(?=IT Certification Guaranteed, The Easy Way!)(.*?)(\d+)', r"",texto_completo,flags=re.MULTILINE)
-
-print(texto_completo)
 
 # Dividimos el texto en las distintas preguntas
 preguntas = texto_completo.split('QUESTION')
@@ -72,7 +70,6 @@ for i, pregunta in enumerate(preguntas[1:], start=1):
     # Busco las respuestas TRUE y añado formato para el CSV:
     m = re.search(r"Answer: .?([A-Z])(?: .?([A-Z]))?(?: .?([A-Z]))?(?: .?([A-Z]))?",pregunta)
     respuestas_correctas = m.groups()
-    print(m.groups())
     g = bool(m)
     if g == True:
         for letra in m.groups():
@@ -83,11 +80,9 @@ for i, pregunta in enumerate(preguntas[1:], start=1):
             pregunta_a_lista=list(pregunta)
             pregunta_a_lista.insert(pos,'"\tTRUE')
             pregunta=''.join(pregunta_a_lista)
-            print(pregunta)
 
     #Quitamos la linea Answer: con las respuestas, ya que ahora las respuestas estan indicadas en las columnas con el valor TRUE 
     pregunta = re.sub(r"(?<=\n)Answer:.+?(\n|$)",r"",pregunta)
-    print(pregunta)
 
     #Buscamos cuantas alternativas tiene la pregunta
     respuestas = re.findall(r"(?<=\n)([A-G])\.",pregunta)
@@ -104,10 +99,7 @@ for i, pregunta in enumerate(preguntas[1:], start=1):
             pregunta_a_lista=list(pregunta)
             pregunta_a_lista.insert(pos,'"\tFALSE')
             pregunta=''.join(pregunta_a_lista)
-            print(pregunta)
         pregunta = re.sub(fr'(?:^|\n)({letra_respuesta}\.)',r'\t"\1', pregunta, flags=re.DOTALL)
-        print(pregunta)
-    print(pregunta)
 
     #Añado una tabulacion y unas dobles comillas antes de la respuesta A para formato del CSV
     m = re.search(r".*?(?=\bA\.)",pregunta)
@@ -116,9 +108,7 @@ for i, pregunta in enumerate(preguntas[1:], start=1):
         pos = m.end()
         pregunta_a_lista=list(pregunta)
         pregunta_a_lista.insert(pos,'\t"')
-        pregunta=''.join(pregunta_a_lista)
-        print(pregunta)
-        
+        pregunta=''.join(pregunta_a_lista) 
 
     # Unimos 'QUESTION' con la pregunta modificada, además le añado dobles comillas y un salto de linea siempre que no sea el primer question
     preguntas[i] = '\"QUESTION' + pregunta
@@ -126,12 +116,11 @@ for i, pregunta in enumerate(preguntas[1:], start=1):
     # Unimos las preguntas de nuevo en un solo string
     texto_modificado = ''.join(preguntas)
 
-    print(texto_modificado)
-
-
-#Finalmente escrivimos el csv, para posterior importarlo en https://www.studysmarter.es/
-with open('resultado.csv', 'w') as res:
-    res.write(texto_modificado)
-print(texto_modificado)
+    #Finalmente escrivimos el csv, para posterior importarlo en https://www.studysmarter.es/
+    
+    with open('resultado.csv', 'w') as res:
+        res.write(texto_modificado)
+        porcentaje = int((i/len(preguntas)) * 100)
+        print(f"Completado: {porcentaje}%", end="\r")
 
 
